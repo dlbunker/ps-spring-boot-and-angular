6ab33c4d028a2ac14414b0f6edcdf96ca41310b2
Dan Bunker <dan.bunker@stgconsulting.com>
2018-02-03 21:25:56 -0700

    sql updates for timestamps

--- sql/bike.sql
+++ sql/bike.sql
@@ -20,10 +20,10 @@ CREATE TABLE
     );
 
 INSERT INTO bike (id, contact, email, model, name, phone, purchase_date, purchase_price)
-  VALUES (1, 1, 'jeff@bikes.com', 'Globo MTB 29 Full Suspension', 'Jeff Miller', '328-443-5555', 'now', '1100');
+  VALUES (1, 1, 'jeff@bikes.com', 'Globo MTB 29 Full Suspension', 'Jeff Miller', '328-443-5555', 4419619200000, '1100');
 INSERT INTO bike (id, contact, email, model, name, phone, purchase_date, purchase_price)
-  VALUES (2, 0, 'samantha@bikes.com', 'Globo Carbon Fiber Race Series', 'Samantha Davis', '448-397-5555', 'now', '1999');
+  VALUES (2, 0, 'samantha@bikes.com', 'Globo Carbon Fiber Race Series', 'Samantha Davis', '448-397-5555', 4419619200000, '1999');
 INSERT INTO bike (id, contact, email, model, name, phone, purchase_date, purchase_price)
-  VALUES (3, 1, 'dave@bikes.com', 'Globo Time Trial Blade', 'Dave Warren', '563-891-5555', 'now', '2100');
+  VALUES (3, 1, 'dave@bikes.com', 'Globo Time Trial Blade', 'Dave Warren', '563-891-5555', 4419619200000, '2100');
 
 INSERT INTO hibernate_sequence (next_val) VALUES (4);
