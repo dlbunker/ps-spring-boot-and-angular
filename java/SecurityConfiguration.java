f1dffcafbc333a42c88f84c95cb8199e928de061
Dan Bunker <dan.bunker@stgconsulting.com>
2018-03-25 16:05:13 -0600

    security config

---
+++ java/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.globomatics.bike.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Value(value = "${auth0.apiAudience}")
+	private String apiAudience;
+	@Value(value = "${auth0.issuer}")
+	private String issuer;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    JwtWebSecurityConfigurer
+        .forRS256(apiAudience, issuer)
+        .configure(http)
+        .authorizeRequests()
+        .antMatchers(HttpMethod.POST, "/api/v1/bikes").permitAll()
+        .antMatchers(HttpMethod.GET, "/api/v1/bikes").permitAll()
+        .antMatchers(HttpMethod.GET, "/api/v1/bikes/**").permitAll()
+        .anyRequest().authenticated();	
+	}
+
+}
